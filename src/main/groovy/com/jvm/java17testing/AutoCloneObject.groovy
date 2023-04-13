2f09ccc84b8627d7394dac1ed9c0eeea276f8044
Roger Diller <roger@flexrentalsolutions.com>
2023-04-13 09:43:19 -0400

    clone stuff

---
+++ src/main/groovy/com/jvm/java17testing/AutoCloneObject.groovy
@@ -0,0 +1,10 @@
+package com.jvm.java17testing
+
+import groovy.transform.AutoClone
+
+@AutoClone
+class AutoCloneObject {
+
+    String name
+
+}
--- src/main/groovy/com/jvm/java17testing/Java17TestingApplication.groovy
+++ src/main/groovy/com/jvm/java17testing/Java17TestingApplication.groovy
@@ -1,13 +1,22 @@
 package com.jvm.java17testing
 
+import groovy.util.logging.Slf4j
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
+@Slf4j('LOG')
 class Java17TestingApplication {
 
 	static void main(String[] args) {
 		SpringApplication.run(Java17TestingApplication, args)
+
+		AutoCloneObject obj1 = new AutoCloneObject(name: 'Roger Diller')
+
+		AutoCloneObject obj2 = obj1.clone();
+
+		LOG.info("obj2 name: " + obj2.name)
+
 	}
 
 }
