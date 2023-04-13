e84e2e60baa7db0bb02779413cecdad2e35aadfe
Roger Diller <roger@flexrentalsolutions.com>
2023-04-13 09:36:56 -0400

    initial commit

---
+++ src/main/groovy/com/jvm/java17testing/Java17TestingApplication.groovy
@@ -0,0 +1,13 @@
+package com.jvm.java17testing
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Java17TestingApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(Java17TestingApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/jvm/java17testing/Java17TestingApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.jvm.java17testing
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Java17TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
